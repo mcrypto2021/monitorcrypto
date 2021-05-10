08f2363f3b85aa4ac43eba5dd45fa82a85fecf54
mcrypto2021 <83570180+mcrypto2021@users.noreply.github.com>
2021-05-10 10:21:22 +0200

    Add files via upload

--- bet/s1.js
+++ bet/s1.js
@@ -9,3 +9,23 @@ for (i = 0; i < close.length; i++) {
         setTimeout(function(){ div.style.display = "none"; }, 600);
     }
 }
+
+
+
+
+var audio = new Audio(),
+    div   = document.getElementById("show");
+audio.src = "https://www.soundjay.com/button/sounds/button-3.mp3";
+
+function play(num)
+{
+    audio.play();
+    
+    div.innerHTML = "This Is Button " + num;
+}
+
+
+function hide()
+{
+    div.innerHTML = "";
+}
